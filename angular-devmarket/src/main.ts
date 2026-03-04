6e5587195eb4e39b1088dd63ded699e4c3b75f3a
Thaísa Raquel <thaisaraquel.dev@gmail.com>
2026-03-04 15:06:19 -0300

    Delete angular-devmarket directory

--- angular-devmarket/src/main.ts
+++
@@ -1,7 +0,0 @@
-import 'zone.js';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(() => {});
-
